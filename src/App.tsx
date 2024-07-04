9895873903a6d32f7164fc97b73a136e9a8dd12e
nirojneupane5 <nirojneupane5@gmail.com>
2024-07-04 15:41:44 +0545

    basic framer motion

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,26 @@
 import "./App.css";
+import { motion } from "framer-motion";
 
 function App() {
   return (
-    <>
-      <h1 className="text-red-500">Hello</h1>
-    </>
+    <div className="flex items-center justify-center h-screen">
+      <motion.div
+        initial={{ rotate: "0deg", scale: 0, y: 0 }}
+        animate={{
+          rotate: "180deg",
+          scale: 1,
+          y: [0, 150, -150, -150, 0],
+        }}
+        transition={{
+          duration: 1,
+          ease: "backInOut",
+          times: [0, 0.25, 0.5, 0.85, 1],
+        }}
+        className="bg-black w-24 h-24 text-white flex items-center justify-center"
+      >
+        Hello
+      </motion.div>
+    </div>
   );
 }
 
